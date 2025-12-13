ec6a6687105c0ab497687a17302e44bf8496ea47
Shakib22011 <shosaain0101@gmail.com>
2025-12-13 09:22:47 +0600

    c

---
+++ JavaScript/first.js
@@ -0,0 +1,13 @@
+console.log("Hello JS!");
+console.log("Its Shakib Hossain."); //console.log prints the result in the browser console
+
+let name="Shakib Hossain";
+console.log(name);
+let age=23;
+console.log(age);
+let isfollow=true;
+console.log(isfollow);
+let x="5"+5+6;
+console.log(x); //first ta string hole porer gula string dhore ney
+let y=1+4+"7";
+console.log(y); //but er ager gula thik e number hisebe dhoira ney
