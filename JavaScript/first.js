9e2fdd0fa6efc32059534949b9dc721e9e6e31cc
Shakib22011 <shosaain0101@gmail.com>
2025-12-18 12:27:26 +0600

    c

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -11,3 +11,45 @@ let x="5"+5+6;
 console.log(x); //first ta string hole porer gula string dhore ney
 let y=1+4+"7";
 console.log(y); //but er ager gula thik e number hisebe dhoira ney
+console.log(typeof y);
+const student = {
+    name : "Shakib Hossain",
+    age : 23,
+    cgpa : 3.80,
+    pass : true,
+};
+student.cgpa=3.85;    //update previous key value
+student.addr="Tangail";  //assign new key:value to const object
+console.log(student);
+document.getElementById("demo").innerHTML=student.name;
+console.log(student["addr"]);
+const date = new Date("2022-03-25");
+console.log(date);
+let a='A';
+console.log(typeof a);
+
+const time = new Date().getHours();
+let greeting;
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+document.getElementById("demo").innerHTML = greeting;
+
+// &&= assigns the right-hand value only if the left-hand value is truthy.
+
+//Numbers other than 0 are truthy.
+
+
+ //||= assigns the right-hand value only if the left-hand value is falsy
+
+
+//  The ??= Operator
+// The Nullish coalescing assignment operator is used between two values.
+
+// If the first value is undefined or null, the second value is assigned.
+
+//Comparing two JavaScript objects always returns false.
