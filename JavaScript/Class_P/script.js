42d1758bf0940c552452c04b798c1130c1cdc2d3
Shakib22011 <shosaain0101@gmail.com>
2025-12-23 12:00:52 +0600

    commit

---
+++ JavaScript/Class_P/script.js
@@ -0,0 +1,9 @@
+console.log("line 1");
+console.log("line 2");
+setTimeout(() => {
+    console.log("line 3");
+},4000);
+console.log("line 4");
+setTimeout(() =>{
+    console.log("line 5");
+},2000);
